9f4d9d28e5116b2768bb222ad78b7d1ad9314844
josh <josh@penguin>
2025-05-31 11:08:18 +0100

    more render

--- app.py
+++ app.py
@@ -707,6 +707,6 @@ if __name__ == "__main__":
 
 # entry point
 def runApp():
-    port = int(os.environ.get("PORT", 5000)) 
+    port = int(os.environ.get("PORT", 8080)) 
     app.run(host="0.0.0.0", port=port)
 
