f6b7be3d892def0bd3b4c4b6cedec7425f5fb67a
josh <josh@penguin>
2025-05-31 10:56:59 +0100

    more trying to get render to run

--- app.py
+++ app.py
@@ -708,5 +708,5 @@ if __name__ == "__main__":
 # entry point
 def runApp():
     port = int(os.environ.get("PORT", 5000)) 
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
 
