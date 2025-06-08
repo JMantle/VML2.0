0bdc881d6035b5f3e94d3bb22ec8661b9c58de86
josh <josh@penguin>
2025-06-08 23:17:38 +0100

    cleaned up debugging

--- bot.py
+++ bot.py
@@ -71,19 +71,10 @@ async def checkEvents():
     while True:
         await asyncio.sleep(10)
 
-        guild = bot.guilds[0]
-        for channel in guild.text_channels:
-            if channel.name == "standings":
-                await channel.send("new events?")
         try:
             cursor, conn = get_db_connection(dictionary=True)
             cursor.execute("SELECT * FROM events")
             events = cursor.fetchall()
-
-            guild = bot.guilds[0]
-            for channel in guild.text_channels:
-                if channel.name == "standings":
-                    await channel.send(events)
 
             teamNames = ["Apex", "Sneaky Snakes", "TFO", "Galaxy Guardians", "Xenon", "741"]
 
